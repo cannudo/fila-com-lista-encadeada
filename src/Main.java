ea89a737723a65eda3cdb781969fdffc8a0be3fc
Luan da Costa Redmann <luandacostaredmann@gmail.com>
2025-09-28 23:12:23 -0300

    O desafio é a fila, meu fi

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("O \"Olá, mundo!\" da gata.");
+        ListaEncadeada<String> lista = new ListaEncadeada<>();
+        System.out.println(lista.toString());
+        
+        Fila foo = new Fila();
+    }
+}
+
+class Fila {
+    public Fila() {
+        System.out.println("Classe Fila diz: \"Olá, mundo!\"");            
     }
 }
