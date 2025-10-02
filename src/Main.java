f7eb3574da579de9b2ebc11818778f9bb623cf1a
Luan da Costa Redmann <luandacostaredmann@gmail.com>
2025-10-02 01:40:49 -0300

    Samba de Um Minuto, Roberta Sá

--- src/Main.java
+++ src/Main.java
@@ -11,5 +11,16 @@ public class Main {
 class Fila {
     public Fila() {
         System.out.println("Classe Fila diz: \"Olá, mundo!\"");            
+        /*
+         * ----------------------
+         * INSERÇÕES E REMOÇÕES
+         * SEGUEM O ESQUEMA FIFO
+         * (FIRST IN, FIRST OUT)
+         * ----------------------
+         * O primeiro dado a entrar 
+         * também será o primeiro dado a sair
+         * da fila.
+         * ----------------------
+        */
     }
 }
