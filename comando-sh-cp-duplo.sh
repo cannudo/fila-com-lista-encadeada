98c757f027d7b3aadccbac2b6db921c4e16d5328
Luan da Costa Redmann <luandacostaredmann@gmail.com>
2025-10-03 20:21:01 -0300

    20 crianças mortas pelo Estado moderno de isHell. Enfileira e desenfileira.

---
+++ comando-sh-cp-duplo.sh
@@ -0,0 +1,2 @@
+javac -cp "lib/lista-encadeada.jar" src/Main.java;java -cp "src:lib/lista-encadeada.jar" Main
+
--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,49 @@
 public class Main {
     public static void main(String[] args) {
-        Fila<String> fila = new Fila<>();
-        fila.enfileirar("Palestina LIVRE");
-        System.out.println(fila.lista.toString());
+        Fila<String> israelVaiTerQuePagarPelasSeguintesVidas = new Fila<>();
+        String[] nomesDasCriancas = {
+            "Razan Jihad Asaliyyah",
+            "Anas Bakri",
+            "Noura Walid Abdulsalam Shaheen",
+            "Maryam Nour Al-Din Wael Daban",
+            "Fatima Louay Rafiq Al-Sultan",
+            "Watan Mohammed Abd Al-Rahim Al-Madhoon",
+            "Mohammad Al-Jabbari Said Misbah Al-Khour",
+            "Diyaa Ahmed Abd Al-Aati Saleh Moussa",
+            "Oday Mohammed Rafiq Al-Sultan",
+            "Mohammed Nidal Hisham Attallah",
+            "Ahmad Shadi Talal Al-Haddad",
+            "Masa Mohammed Youssef Nasr",
+            "Ayat Abd Al-Aziz Omar Farwana",
+            "Maalek Mohammed Shafeeq Abu-Al-Kas",
+            "Sarah Abd Al-Rahman Mohammed Hamad",
+            "Mohammed Saleh Mahmoud Al-Deiri",
+            "Mecca Ahmed Eid Abu-Sherekh",
+            "Iyad Abd Al-Rahman Jihad Muhaysen",
+            "Adam Mohammed Sameer Abu-Ajwah",
+            "Alayan Abd Al-Rahman Alayan Al-Ashqar"
+        };
+
+        for(int i = 0; i < nomesDasCriancas.length; i++) {
+            israelVaiTerQuePagarPelasSeguintesVidas.enfileirar(nomesDasCriancas[i]);
+        }
+
+        System.out.println(israelVaiTerQuePagarPelasSeguintesVidas.lista.toString());
+
+        for(int i = 0; i < nomesDasCriancas.length; i++) {
+            israelVaiTerQuePagarPelasSeguintesVidas.desenfileirar();
+        }
     }
 }
 
 class Fila<TipoGenerico> {
     ListaEncadeada<TipoGenerico> lista;
+    int quantidade;
+    
     public Fila() {
         System.out.println("Classe Fila diz: \"Olá, mundo!\"");            
         this.lista = new ListaEncadeada<>();
+        this.quantidade = 0;
         /*
          * ----------------------
          * INSERÇÕES E REMOÇÕES
@@ -33,12 +66,33 @@ class Fila<TipoGenerico> {
         // Verifique se você pode pedir que ela informe sobre a ordem dos nós.
         No<TipoGenerico> novoDado = new No<>(dado);
         this.lista.adicionarNoAoFimDaLista(novoDado);
+        this.incrementarQuantidade();
+        System.out.println("Quantidade de dados na fila: " + this.getQuantidade());
     }
 
     public void desenfileirar() {
         // Disse isso porque
         // ele deve ser também o primeiro dado
-        // a ser removido da fila.
+        // a ser removido da fila.;
+        this.lista.removerUltimoNo();
+        this.decrementrarQuantidade();
+        System.out.println("Quantidade de dados na fila: " + this.getQuantidade());
+    }
+
+    public void incrementarQuantidade() {
+        this.quantidade++;
+    }
+
+    public void decrementrarQuantidade() {
+        this.quantidade--;
+    }
+
+    public int getQuantidade() {
+        return this.quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
     }
 
     public String PalestinaLivre() {
